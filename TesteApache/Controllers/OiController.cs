50f9089edd705bc7d244bb295383b62cf8a4a9a1
Gabriel Richard <gabrielrichard.contato@gmail.com>
2024-12-30 15:19:36 -0300

    teste

---
+++ TesteApache/Controllers/OiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesteApache.Controllers;
+
+public class OiController : ControllerBase
+{
+    [HttpGet("oi")]
+    public async Task<IActionResult> Oi()
+    {
+        return Ok("oi");
+    }
+    [HttpGet("tchau")]
+    public async Task<IActionResult> tchau()
+    {
+        return Ok("tchau");
+    }
+    
+}
